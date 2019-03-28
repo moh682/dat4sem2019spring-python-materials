e7aaf7fa19f9afb69b007f36b1da0e502d687b64
Thomas-Hartmann <thomas@webtrade.dk>
2019-03-28 15:29:18 +0100

    update to selenium_krak.py now the real one this time - tested and working

--- selenium_krak.py
+++ selenium_krak.py
@@ -10,35 +10,38 @@ def get_info(name):
     browser.get(base_url)
     browser.implicitly_wait(3)
 
-    search_field = browser.find_element_by_id('e-what-all')
+    search_field = browser.find_element_by_tag_name('input')
     search_field.send_keys(name)
     search_field.submit()
 
     sleep(3)
     # browser.implicitly_wait(3)
 
-    link_to_persons = browser.find_elements_by_link_text('Personer')
+    link_to_persons = browser.find_elements_by_partial_link_text('Personer')
+    #print('PERSONS: ',link_to_persons)
     link_to_persons[0].click()
 
     sleep(3)
 
     page_source = browser.page_source
 
-    soup = bs4.BeautifulSoup(page_source, 'html5lib')
-    event_cells = soup.find_all('div', {'class': 'hit-header-block-center'})
-
+    soup = bs4.BeautifulSoup(page_source, 'html.parser')
+    event_cells = soup.find_all('div', {'class': 'topBlock'})
+    
     entries_str = []
     for e in event_cells:
-        lines = e.text.splitlines()
-        lines = [l for l in lines if l]
-
-        entry_str = '\n'.join(lines[:-3])
-        entries_str.append(entry_str)
-
+        # print('cell: ',e)
+        name = e.select('div h3 a')[0].text
+        street = e.select('div>span:nth-child(1)')[0].text
+        city = e.select('div>span:nth-child(2)')[0].text
+        phone = e.select('span>span>div')[0].text
+        samlet = '{}\n{}\n{}\n{}\n'.format(name,street,city,phone)
+        print(samlet)
+        # print(element.text)
+        entries_str.append(samlet)
     return entries_str
 
-
-def save_to_file(content, out_path='./addresses.txt'):
+def save_to_file(content, out_path='./test.txt'):
     with open(out_path, 'w') as f:
         f.write(content)
 
